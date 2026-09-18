a05cae3122ee8504f7fd0dc5ab08f1a3a814650c
Aryan ved <yadhuvanshiaryan@gmail.com>
2026-09-18 13:31:52 +0530

    Add Python basics

---
+++ PythonBasics/conditionals in python/cond1.py
@@ -0,0 +1,8 @@
+# you are creating a notification system for a smart Kettle.
+# it should remind the user only when the kettle has finished boiling. Task:
+# A variable kettle_boiled = true, if boiled show "kettle done ! Time to make chai! "
+
+kettle_boiled = True
+
+if kettle_boiled:
+    print(f"kettle done ! Time to make chai!")
