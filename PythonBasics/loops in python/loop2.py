b0cdd344f5b77fecdf707dafec40ce8ae0f3227c
Aryan ved <yadhuvanshiaryan@gmail.com>
2026-09-19 23:47:25 +0530

    Add simulation for chai batch preparation using range

---
+++ PythonBasics/loops in python/loop2.py
@@ -0,0 +1,7 @@
+# A chai shop makes tea in batches every 15 min, you want to simulate 4 batches.
+# Task use range() to simulate batch numbers.
+
+#  use range to simulate batch number and print preparing chai for batch #number.
+
+for batches in range(1,5):
+    print(f"preparing chai for batch #{batches}")
