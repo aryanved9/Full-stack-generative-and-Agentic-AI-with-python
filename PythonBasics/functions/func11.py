fab24aca2db1d91a408e7309125e2c3b6a40d06b
Aryan ved <yadhuvanshiaryan@gmail.com>
2026-09-24 12:54:57 +0530

    Add new file for function import examples

---
+++ PythonBasics/functions/func11.py
@@ -0,0 +1,6 @@
+#  Importing ways of functions/method from another file.
+
+# 1. import chai_recipe.py  -> chai_recipe.brew() -> importing whole file might containing so many function which can accessible using dot "."
+# 2. from chai_recipe.py import brew  -> brew() -> import exact what you need and excute it.
+# 3. from chai_recipe.py import brew as start_brewing -> when you already have same method defined in your file then use "as" -> start_brewing()
+
